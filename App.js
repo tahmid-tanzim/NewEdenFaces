ee4fdd4718a74f525b4f66c8012da45c8f2b84a6
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2016-09-03 11:04:32 +0600

    React Fundamentals: Introduction to Properties

--- App.js
+++ App.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 class App extends React.Component {
     render() {
-        return <div>Hello</div>
+        const {name, age} = this.props;
+        return (
+            <div>
+                <h2>Hello</h2>
+                <h4>{name} <small>Age: {age}</small></h4>
+                <p>Lorem Ipsum 2 Ipsum Lorem Ipsum</p>
+            </div>
+        );
     }
 }
+
+App.propTypes = {
+    name: React.PropTypes.string,
+    age: React.PropTypes.number.isRequired
+};
+
+App.defaultProps = {
+    name: 'Unknown',
+    age: 0
+};
 
 export default App;
--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+    <App name="Tahmid Tanzim Lupin"
+         age="25" />,
+    document.getElementById('app')
+);
